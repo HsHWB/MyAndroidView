4830e85659ebd6a0d61b85032087a72dae56d0cd
HsHWB <Hswbinforgithub@gmail.com>
2014-11-03 12:26:53 +0800

    a view
    a view which is using paint and canvas

--- app/src/main/java/example/com/textproject1/Message.java
+++ app/src/main/java/example/com/textproject1/Message.java
@@ -10,14 +10,16 @@ public class Message extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        NewListView nLv = new NewListView(this,this);
+//        NewListView nLv = new NewListView(this,this);
 //        setContentView(R.layout.newlistviewxml);
+//        setContentView(nLv);
+//        nLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(Message.this,"击中"+String.valueOf(position),Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        NewListView2 nLv = new NewListView2(this,null);
         setContentView(nLv);
-        nLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(Message.this,"击中"+String.valueOf(position),Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
---
+++ app/src/main/java/example/com/textproject1/NewListView2.java
@@ -0,0 +1,82 @@
+package example.com.textproject1;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * @author hs
+ * android自定义View，重写构造函数、 onDraw， (onMeasure)等函数
+ * 如果自定义的View需要有自定义的属性，需要在values下建立attrs.xml。在其中定义你的属性。
+ * 在使用到自定义View的xml布局文件中需要加入xmlns:前缀="http://schemas.android.com/apk/res/你的自定义View所在的包路径".
+ * 在使用自定义属性的时候，使用前缀：属性名，如my:textColor="#FFFFFFF"。
+ */
+public class NewListView2 extends View {
+    /**
+     * 定义画笔
+     */
+    Paint mPaint;//画笔,包含了画几何图形、文本等的样式和颜色信息
+
+    public NewListView2(Context context){
+        super(context);
+    }
+
+    public NewListView2(Context context, AttributeSet attrs){
+        super(context, attrs);
+        mPaint = new Paint();
+        /**
+         * TypedArray是一个数组容器,在使用完成后，一定要调用recycle方法,否则这次的设定会对下次的使用造成影响
+         * 在xml 文件里定义控件的属性，我们已经习惯了android:attrs="" ,那么我们能
+         * 不能定义自己的属性能，比如:test:attrs="" 呢？答案是肯定的.
+         */
+        TypedArray tA = context.obtainStyledAttributes(attrs, R.styleable.NewListView2 );
+        int textColor = tA.getColor(R.styleable.NewListView2_listColor2, 0XFF00FF00); //提供默认值，放置未指定
+        float textSize = tA.getDimension(R.styleable.NewListView2_listSize2, 36);
+        mPaint.setTextSize(textSize);
+        mPaint.setColor(textColor);
+        tA.recycle();
+    }
+
+    /**
+     * @param canvas
+     * Canvas中含有很多画图的接口，利用这些接口，我们可以画出我们想要的图形
+     */
+    public void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        ImageView image = (ImageView)findViewById(R.drawable.sucai1);
+        InputStream iS = null;
+        try {
+            iS = new FileInputStream(file);
+        }catch(Exception e){
+            System.out.println("文件输入流错误");
+        }
+        Bitmap bitmap = BitmapFactory.decodeStream(iS);
+        Rect src = new Rect();// 这个是表示绘画图片的大小
+        Rect dst = new Rect();// 屏幕位置及尺寸
+        src.left = 0;
+        src.top = 0;
+        src.right = bitmap.getWidth();//这个是桌面图的宽度，
+        src.bottom = bitmap.getHeight()/2;//这个是桌面图的高度的一半
+        dst.left = 0;
+        dst.top = 0;
+        dst.right = bitmap.getWidth();;    //表示需绘画的图片的右上角
+        dst.bottom = bitmap.getHeight()/2;    //表示需绘画的图片的右下角
+        canvas.drawBitmap(bitmap,src,dst,mPaint);
+        src = null;
+        dst = null;
+    }
+
+}
