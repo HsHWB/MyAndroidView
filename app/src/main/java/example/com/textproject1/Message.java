68892b2400c36788eee5527a332554aa9c9deb04
HsHWB <Hswbinforgithub@gmail.com>
2014-11-22 16:34:16 +0800

    try to add view

--- app/src/main/java/example/com/textproject1/Message.java
+++ app/src/main/java/example/com/textproject1/Message.java
@@ -3,6 +3,7 @@ package example.com.textproject1;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Toast;
@@ -14,7 +15,6 @@ public class Message extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        NewListView nLv = new NewListView(this,this);
         displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 //        float density = displayMetrics.density;//获取屏幕密度（像素比例）
@@ -25,6 +25,11 @@ public class Message extends Activity {
 //        int screenWidthDip = (int)(displayMetrics.widthPixels);//屏幕宽
 //        int screenHeightDip = (int)(displayMetrics.heightPixels);//屏幕长
 //        System.out.println("屏幕长："+screenHeightDip+"     屏幕宽："+screenWidthDip);
-        setContentView(R.layout.newlistviewxml);
+        setContentView(R.layout.newview);
+        int i = 3;
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.newview,null);
+        while(i-- > 0){
+        }
     }
 }
